f44b22334422dfa5323002e72a438263d5e8852b
cyh-ai <cyh970511@163.com>
2023-02-17 16:54:02 +0800

    feat: 首次创建项目

---
+++ integration-batch/src/main/java/com/integration/batch/BatchApplication.java
@@ -0,0 +1,19 @@
+package com.integration.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cyh
+ * @date 2023.02.17
+ * 批处理
+ */
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+
+
+}
